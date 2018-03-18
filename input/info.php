bf5cf6356baea48b778224fda044e589c61ae297
Xu Wang <>
2018-03-18 12:31:02 +0100

    add movie pic upload

---
+++ input/info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fbicia
+ * Date: 2018/3/17
+ * Time: 下午3:33
+ */
+
+echo phpinfo();
--- input/inputMovieRoles.php
+++ input/inputMovieRoles.php
@@ -8,21 +8,35 @@
 
 
 include_once "connect.php";
+include_once "functions.php";
 
 $movieTitle = $_POST["movieTitle"];
-$posterV = $_POST["posterV"];
-$posterH = $_POST["posterH"];
 $movieDescription = $_POST["movieDescription"];
+$posterH = $_FILES['posterH'];
+$posterV = $_FILES['posterV'];
 $status = $_POST["status"];
 $type = $_POST["type"];
 $director = $_POST["director"];
 $roleNames = $_POST["roleName"];
 $roleDescriptions = $_POST["roleDescription"];
+$today = date("Ymdhis");
+
+#用时间戳重命名文件
+$posterVName= 'v'.$today;
+$posterHName= 'h'.$today;
+
+imgUpload($posterV,'posterV',$posterVName);
+imgUpload($posterH,'posterH',$posterHName);
+
+#文件后缀名
+$v = end(explode(".", $posterV["name"]));
+$h = end(explode(".", $posterH["name"]));
+
 
 
 $actorsSql = 'INSERT INTO `movies` (`title`, `posterV`, `posterH`, `description`, `released`, `type`, `director`) VALUES ( ?, ?, ?, ?, ?, ?, ?);';
 $stmt = $pdo->prepare($actorsSql);
-$stmt->execute(array($movieTitle,$posterV,$posterH,$movieDescription,$status,$type,$director));
+$stmt->execute(array($movieTitle,$posterVName.".".$v,$posterHName.".".$h,$movieDescription,$status,$type,$director));
 #last movie Id
 $movieId = $pdo->lastInsertId();
 
@@ -36,7 +50,6 @@ foreach ($roleNames as $roleName){
     $rolesSql = 'INSERT INTO `roles` (`movieId`, `name`, `description`) VALUES (?, ?, ?);';
     $stmt = $pdo->prepare($rolesSql);
     $stmt->execute(array($movieId,$roleName,$roleDescription));
-
 
     $n++;
 }
