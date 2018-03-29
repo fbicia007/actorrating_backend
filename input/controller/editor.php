d1b7562d0dc8d19f41f868fbae9c678996a20220
Xu Wang <>
2018-03-29 23:08:09 +0200

    +actor editor function

--- input/controller/editor.php
+++ input/controller/editor.php
@@ -6,12 +6,30 @@
  * Time: 下午10:12
  */
 
+include_once "../connect.php";
+
     if($_GET['actorId'])
     {
-        echo "编辑演员";
+        $actorId = $_GET['actorId'];
+        $actorName = $_POST["actorName"];
+        $birthday = $_POST["birthday"];
+        $constellation = $_POST["constellation"];
+        $birthplace = $_POST["birthplace"];
+        $profession = $_POST["profession"];
+        $actorDescription = $_POST["actorDescription"];
+        $photoName = $_POST['photoName'];
+
+        $actorsSql = 'UPDATE `actors` SET `name` = ?, `photo` = ?, `description` = ?, `birthday` = ?, `constellation` = ?, `birthplace` = ?, `profession` = ? WHERE `id` = ?;';
+        $stmt = $pdo->prepare($actorsSql);
+        $stmt->execute(array($actorName,$photoName,$actorDescription,$birthday,$constellation,$birthplace,$profession,$actorId));
+
+        header("Location:../alist.php");
+        exit();
 
     }
     elseif ($_GET['movieId']&&$_GET['status'])
     {
-        echo "编辑movie";
+        $status = $_GET['status'];
+        $movieId = $_GET['movieId'];
+        header("Location:../movieEditor.php?status=".$status."&movieId=".$movieId);
     }
